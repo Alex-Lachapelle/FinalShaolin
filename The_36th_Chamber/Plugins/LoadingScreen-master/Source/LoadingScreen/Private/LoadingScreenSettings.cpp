dd5ce123c1a69b48cce83891ad98e9280bb2d29b
Alex-Lachapelle <alex.lachapelle@gmail.com>
2016-04-07 22:58:46 -0400

    Changed dmg system and added loading screens

---
+++ The_36th_Chamber/Plugins/LoadingScreen-master/Source/LoadingScreen/Private/LoadingScreenSettings.cpp
@@ -0,0 +1,41 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "LoadingScreenPrivatePCH.h"
+#include "LoadingScreenSettings.h"
+
+#define LOCTEXT_NAMESPACE "LoadingScreen"
+
+FLoadingScreenDescription::FLoadingScreenDescription()
+	: MinimumLoadingScreenDisplayTime(-1)
+	, bAutoCompleteWhenLoadingCompletes(true)
+	, bMoviesAreSkippable(true)
+	, bWaitForManualStop(false)
+	, bShowUIOverlay(true)
+	, LoadingText(LOCTEXT("Loading", "LOADING"))
+	, ImageStretch(EStretch::ScaleToFit)
+{
+	if ( !UE_SERVER )
+	{
+		static ConstructorHelpers::FObjectFinder<UFont> RobotoFontObj(TEXT("/Engine/EngineFonts/Roboto"));
+		LoadingFont = FSlateFontInfo(RobotoFontObj.Object, 32, FName("Bold"));
+	}
+}
+
+bool FLoadingScreenDescription::IsValid() const
+{
+	return MoviePaths.Num() > 0 || Images.Num() > 0;
+}
+
+ULoadingScreenSettings::ULoadingScreenSettings(const FObjectInitializer& Initializer)
+	: Super(Initializer)
+{
+	TipWrapAt = 1000.0f;
+
+	if ( !UE_SERVER )
+	{
+		static ConstructorHelpers::FObjectFinder<UFont> RobotoFontObj(TEXT("/Engine/EngineFonts/Roboto"));
+		TipFont = FSlateFontInfo(RobotoFontObj.Object, 20, FName("Normal"));
+	}
+}
+
+#undef LOCTEXT_NAMESPACE
